a87a35221c5dd973b75ac7ceac5e4ce503d2b1fd
Dhaval <dp9718776@gmail.com>
2025-06-30 10:22:30 +0530

    Update

--- src/Utility.h
+++ src/Utility.h
@@ -2,18 +2,22 @@
 
 inline ImVec4 ColorToImVec4(const Color &C)
 {
-	return ImVec4(
+	return ImVec4
+	(
 		static_cast<float>(C.r) / 255.0f,
 		static_cast<float>(C.g) / 255.0f,
 		static_cast<float>(C.b) / 255.0f,
-		static_cast<float>(C.a) / 255.0f);
+		static_cast<float>(C.a) / 255.0f
+	);
 }
 
 inline Color ImVec4ToColor(const ImVec4 &V)
 {
-	return Color{
+	return Color
+	{
 		static_cast<unsigned char>(V.x * 255.0f),
 		static_cast<unsigned char>(V.y * 255.0f),
 		static_cast<unsigned char>(V.z * 255.0f),
-		static_cast<unsigned char>(V.w * 255.0f)};
+		static_cast<unsigned char>(V.w * 255.0f)
+	};
 }
